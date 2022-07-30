5ad617b04a3aed79d1626ac773b93c4e5eef0a25
Johannes <84457538+p-jeff@users.noreply.github.com>
2022-07-30 16:59:12 +0200

    Created pages

---
+++ src/routes/EntryButton.svelte
@@ -0,0 +1,72 @@
+<script>
+	export let label;
+	export let link;
+	export let enabled;
+
+	function clickHandler() {
+		window.location = link;
+	}
+</script>
+
+{#if enabled}
+	<button on:click={clickHandler} class="button-74"> {label} </button>
+{:else}
+	<button class="buttonDiabled"> {label} </button>
+{/if}
+
+<style>
+	button {
+		margin: 5%;
+		font: 1.5em Raleway;
+	}
+
+	.buttonDiabled {
+		background-color: rgb(214, 100, 100);
+		border: 2px solid #422800;
+		border-radius: 0px;
+		text-decoration: line-through;
+		color: #422800;
+		cursor: pointer;
+		display: inline-block;
+		font-weight: 600;
+		font-size: 18px;
+		padding: 0 18px;
+		line-height: 50px;
+		text-align: center;
+	}
+
+	.button-74 {
+		background-color: aquamarine;
+		border: 2px solid #422800;
+		border-radius: 0px;
+		box-shadow: #616161 4px 4px 0 0;
+		color: #422800;
+		cursor: pointer;
+		display: inline-block;
+		font-weight: 600;
+		font-size: 18px;
+		padding: 0 18px;
+		line-height: 50px;
+		text-align: center;
+		text-decoration: none;
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
+	}
+
+	.button-74:hover {
+		background-color: #fff;
+	}
+
+	.button-74:active {
+		box-shadow: #422800 2px 2px 0 0;
+		transform: translate(2px, 2px);
+	}
+
+	@media (min-width: 768px) {
+		.button-74 {
+			min-width: 120px;
+			padding: 0 25px;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import EntryButton from './EntryButton.svelte';
+	import '../global.css';
+
+	let Entries = [
+		{ label: 'McBoaty', link: 'https://p-jeff.github.io/McBoaty/', enabled: true },
+		{ label: 'Music Visalizer', link: 'https://p-jeff.github.io/MusicVisBox/', enabled: false },
+		{ label: 'GaBot', link: 'https://p-jeff.github.io/gabot/', enabled: false }
+	];
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway" />
+</svelte:head>
+
+<div class="left">
+	{#each Entries as Entry}
+		<EntryButton {...Entry} />
+	{/each}
+</div>
+
+<h3>
+	Hey, I'm Johannes, studying Speculative Design at the University of applied Arts Vienna. In my
+	free time I tinker around with code to create web based pieces. This is just the landing page
+	linking my ideas :) <br />
+</h3>
+
+<h3 class="red">
+	I am currently restructuring my GitHub which is why some are diabled at the moment
+</h3>
+
+<style>
+	h3 {
+		text-align: right;
+		max-width: 50%;
+		margin-left: 40%;
+
+		font: 1.5em Raleway;
+	}
+	.red {
+		color: rgb(214, 100, 100);
+	}
+	.left {
+		text-align: left;
+		max-width: 50%;
+		margin-bottom: 20%;
+		margin-left: 10%;
+		font: 1em Raleway;
+	}
+</style>
